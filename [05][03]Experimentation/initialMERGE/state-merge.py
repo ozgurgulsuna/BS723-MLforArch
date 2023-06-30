6fd32ba3e0ca5f54f2903fdd99de4bcca330471e
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2023-06-30 18:20:49 +0300

    [05][03] merge, TSUCS1 and YuWang
    this parameters for X plane merger works but somehow others wont

--- [05][03]Experimentation/initialMERGE/state-merge.py
+++ [05][03]Experimentation/initialMERGE/state-merge.py
@@ -15,9 +15,9 @@ from scipy.integrate import odeint
 # 2. Yu-Wang Attractor
 #----------------------------------------------------------#
 
-solver = 'euler'
+# solver = 'euler'
 dt = 0.005
-t = np.arange(0, 10, dt)
+t = np.arange(0, 0.005*5000, dt)
 
 #----------------------------------------------------------#
 # TSUCS 1 Attractor (Three-Scroll Unified Chaotic System)
@@ -35,8 +35,8 @@ f = 20
 
 # Yu-Wang Attractor
 # Initial values
-x1 = 0.1
-y1 = 0.1
+x1 = 2
+y1 = 0.5
 z1 = 0.1
 
 # Parameters
@@ -57,24 +57,45 @@ def YuWang(state, t):
     x, y, z = state
     return a1*(y-x), b1*x-c1*x*z, np.exp(x*y)-d1*z
 
+# def Merge(state,t):
+#     x, y, z = state
+#     if (t < 10):
+#         return a * (y - x) + d * x * z, f * y - x * z , c * z + x * y - e * x**2
+#     else:
+#         return a1*(y-x), b1*x-c1*x*z, np.exp(x*y)-d1*z
+    
+
+def Merge(state,t):
+    x, y, z = state
+    if (x < 0):
+        return a * (y - x) + d * x * z, f * y - x * z , c * z + x * y - e * x**2
+    else:
+        return a1*(y-x), b1*x-c1*x*z, np.exp(x*y)-d1*z
+    
+
 #----------------------------------------------------------#
 # Solve the systems
 TSUCS1 = odeint(TSUCS1, (x0, y0, z0), t)
 
 YuWang = odeint(YuWang, (x1, y1, z1), t)
 
+Merge = odeint(Merge, (x1, y1, z1), t)
+# np.delete(Merge[:,0],-1)
+# np.delete(Merge[:,1],-1)
+# np.delete(Merge[:,2],-1)
 #----------------------------------------------------------#
 
 # Plot the system
-print("Plotting the system...")
-plt.figure()
-ax = plt.axes(projection='3d')
 
-ax.plot3D(TSUCS1[:, 0], TSUCS1[:, 1], TSUCS1[:, 2])
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-plt.show()
+# print("Plotting the system...")
+# plt.figure()
+# ax = plt.axes(projection='3d')
+
+# ax.plot3D(TSUCS1[:, 0], TSUCS1[:, 1], TSUCS1[:, 2])
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+# plt.show()
 
 #----------------------------------------------------------#
 
@@ -83,10 +104,26 @@ plt.show()
 #----------------------------------------------------------#
 
 # Plot the system
+
+# plt.figure()
+# ax = plt.axes(projection='3d')
+
+# ax.plot3D(YuWang[:, 0], YuWang[:, 1], YuWang[:, 2])
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+# plt.show()
+
+#----------------------------------------------------------#
+
+
+# Plot the system
+print("Plotting the system...")
 plt.figure()
 ax = plt.axes(projection='3d')
 
-ax.plot3D(YuWang[:, 0], YuWang[:, 1], YuWang[:, 2])
+ax.plot3D(Merge[:, 0], Merge[:, 1], Merge[:, 2])
+# ax.scatter3D(Merge[:, 0], Merge[:, 1], Merge[:, 2], color="green")
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
@@ -94,13 +131,6 @@ plt.show()
 
 #----------------------------------------------------------#
 
-# TSUCS 1 Attractor (Three-Scroll Unified Chaotic System)
-
-#----------------------------------------------------------#
-
-# Plot the system
-
-fig = plt.figure()
 
 
 
