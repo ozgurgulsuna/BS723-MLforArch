d2a97536f6787c23ae5cba8b4ef7c08269cf1c53
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2023-07-01 00:13:00 +0300

    [05][03] Normalization with mySolver

--- [05][03]Experimentation/initialMERGE/state-merge.py
+++ [05][03]Experimentation/initialMERGE/state-merge.py
@@ -15,16 +15,58 @@ from scipy.integrate import odeint
 # 2. Yu-Wang Attractor
 #----------------------------------------------------------#
 
+#----------------------------------------------------------#
+# Define Transformations
+
+def Normalize(state, scale, offset):
+    x, y, z = state
+    scale_x, scale_y, scale_z = scale
+    offset_x, offset_y, offset_z = offset
+
+    x = x - offset_x
+    y = y - offset_y
+    z = z - offset_z
+
+    x = x/scale_x
+    y = y/scale_y
+    z = z/scale_z
+
+    return x,y,z
+
+def Expand(state, scale, offset):
+    x, y, z = state
+    scale_x, scale_y, scale_z = scale
+    offset_x, offset_y, offset_z = offset
+
+    x = x * scale_x
+    y = y * scale_y
+    z = z * scale_z
+
+    x = x + offset_x
+    y = y + offset_y
+    z = z + offset_z
+
+    return x,y,z
+
+
+
+
 # solver = 'euler'
-dt = 0.005
-t = np.arange(0, 0.005*5000, dt)
+dt = 0.001
+t = np.arange(0, dt*20000, dt)
 
 #----------------------------------------------------------#
 # TSUCS 1 Attractor (Three-Scroll Unified Chaotic System)
 # Initial values
-x0 = 0.1
-y0 = 0.1
-z0 = 0.1
+
+
+x0 = 0.0016871012813302357
+y0 = -0.0014740084235375197
+z0 = -0.8014225096271048
+
+x0,y0,z0 = Normalize((0.1,0.1,0.1),(136.6145132443005,132.93521309671516,83.11710323466052), (0.016690981504353886,-0.054503652638672406,33.21631003781814))
+
+
 
 # Parameters
 a = 40
@@ -35,8 +77,8 @@ f = 20
 
 # Yu-Wang Attractor
 # Initial values
-x1 = 2
-y1 = 0.5
+x1 = 0.1
+y1 = 0.1
 z1 = 0.1
 
 # Parameters
@@ -44,7 +86,6 @@ a1 = 10
 b1 = 40
 c1 = 2
 d1 = 2.5
-
 
 #----------------------------------------------------------#
 # Define the systems
@@ -66,12 +107,56 @@ def YuWang(state, t):
     
 
 def Merge(state,t):
-    x, y, z = state
-    if (x < 0):
-        return a * (y - x) + d * x * z, f * y - x * z , c * z + x * y - e * x**2
-    else:
-        return a1*(y-x), b1*x-c1*x*z, np.exp(x*y)-d1*z
+    # x,y,z = state
+    x,y,z = Expand(state, (136.6145132443005,132.93521309671516,83.11710323466052), (0.016690981504353886,-0.054503652638672406,33.21631003781814))
+
+    dx = (a * (y - x) + d * x * z)
+    dy = (f * y - x * z)
+    dz = (c * z + x * y - e * x**2)
+
+    x = dx*dt+x
+    y = dy*dt+y
+    z = dz*dt+z
+
+
+    x,y,z = Normalize((x,y,z),(136.6145132443005,132.93521309671516,83.11710323466052), (0.016690981504353886,-0.054503652638672406,33.21631003781814))
+    return x,y,z
+
+    # else:
+    #     x,y,z = Expand(state, (136.0415254952852+1,133.11375168220457+1,82.92317366334211+1), (-0.1295158319772156,0.29621079126825123,66.55649794351987))
+
+    #     x = a1*(y-x)
+    #     y = b1*x-c1*x*z
+    #     z = np.exp(x*y)-d1*z
+
+    #     x,y,z = Normalize((x,y,z), (136.0415254952852+1,133.11375168220457+1,82.92317366334211+1), (-0.1295158319772156,0.29621079126825123,66.55649794351987))
+    #     return a1*(y/10-x/10), b1*x/10-c1*x/10*z/10, np.exp(x*y/100)-d1*z/10
     
+# def Merge(state,t):
+#     x, y, z = state
+#     if (x > 0) :
+#         return a * (y - x) + d * x * z, f * y - x * z , c * z + x * y - e * x**2
+#     else:
+#         return a*1.04 * (y - x) + d*1.04 * x * z, f*1.04 * y - x * z , c * z + x * y - e*1.04 * x**2
+
+#----------------------------------------------------------#
+# mysolver
+def mysolver(system, state, t):
+    x, y, z = state
+    output = np.empty((1,3))
+    for i in range(len(t)):
+        x, y, z = system((x, y, z), t[i])
+        output = np.append(output, [[x,y,z]], axis=0)
+        # print(x,y,z)
+
+    return output
+
+
+myTSUCS1 = mysolver(Merge, (x0, y0, z0), t)
+myTSUCS1 = np.delete(myTSUCS1, 0, 0)
+
+
+
 
 #----------------------------------------------------------#
 # Solve the systems
@@ -79,7 +164,7 @@ TSUCS1 = odeint(TSUCS1, (x0, y0, z0), t)
 
 YuWang = odeint(YuWang, (x1, y1, z1), t)
 
-Merge = odeint(Merge, (x1, y1, z1), t)
+Merge = odeint(Merge, (x0, y0, z0), t)
 # np.delete(Merge[:,0],-1)
 # np.delete(Merge[:,1],-1)
 # np.delete(Merge[:,2],-1)
@@ -87,15 +172,15 @@ Merge = odeint(Merge, (x1, y1, z1), t)
 
 # Plot the system
 
-# print("Plotting the system...")
-# plt.figure()
-# ax = plt.axes(projection='3d')
+print("Plotting the system...")
+plt.figure()
+ax = plt.axes(projection='3d')
 
-# ax.plot3D(TSUCS1[:, 0], TSUCS1[:, 1], TSUCS1[:, 2])
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_zlabel('z')
-# plt.show()
+ax.plot3D(TSUCS1[:, 0], TSUCS1[:, 1], TSUCS1[:, 2])
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+plt.show()
 
 #----------------------------------------------------------#
 
@@ -118,11 +203,25 @@ Merge = odeint(Merge, (x1, y1, z1), t)
 
 
 # Plot the system
+# print("Plotting the system...")
+# plt.figure()
+# ax = plt.axes(projection='3d')
+
+# ax.plot3D(Merge[:, 0], Merge[:, 1], Merge[:, 2])
+# # ax.scatter3D(Merge[:, 0], Merge[:, 1], Merge[:, 2], color="green")
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+# plt.show()
+
+#----------------------------------------------------------#
+
+# Plot the system
 print("Plotting the system...")
 plt.figure()
 ax = plt.axes(projection='3d')
 
-ax.plot3D(Merge[:, 0], Merge[:, 1], Merge[:, 2])
+ax.plot3D(myTSUCS1[:, 0], myTSUCS1[:, 1], myTSUCS1[:, 2])
 # ax.scatter3D(Merge[:, 0], Merge[:, 1], Merge[:, 2], color="green")
 ax.set_xlabel('x')
 ax.set_ylabel('y')
@@ -130,8 +229,6 @@ ax.set_zlabel('z')
 plt.show()
 
 #----------------------------------------------------------#
-
-
 
 
 
